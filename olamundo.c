647a14f16bf397426df2e43ae1ebe517e3c84562
Kleiton013 <202507335961@alunos.estacio.br>
2025-08-15 00:36:52 -0300

    1 projecto

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main {
+    int idade, matricula;
+    float altura;
+    char nome[20];
 
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: \n");
+    scanf("%s", &nome);
+
+    prinf("Digite sua matricula: \n");
+    scanf("%d", &matricula);
+  
+    
+
+    
+}
